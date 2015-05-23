4229b6ef2028fb66fb40a5535afa592c3a0936c3
enlighter <mandal.sushovan92@gmail.com>
2015-05-23 10:31:43 +0530

    add uniform meta-information for all current modules

---
+++ maths/vlnm.py
@@ -0,0 +1,9 @@
+''' Very Large Number Multiplicator
+	based on "Vedic Mathematics"
+
+	__author__: Suhovan Mandal
+	__license__: "MIT license"
+	__email__: mandal.sushovan92@gmail.com
+	__site__: www.github.com/enlighter
+'''
+
--- utils/quicksort.py
+++ utils/quicksort.py
@@ -2,6 +2,7 @@
  	__author__: "Sushovan Mandal"
     __license__: "MIT license"
     __email__: "mandal.sushovan92@gmail.com"
+    __site__: www.github.com/enlighter
 '''
 
 from random import randrange
