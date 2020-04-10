b956e897201d9513f96c4d1d6458dbf3528a8a94
enlighter <mandal.sushovan92@gmail.com>
2020-04-10 18:09:02 +0530

    neatly package the different ways merge sort is implemented

--- utils/mergesort.py
+++ utils/mergesort.py
@@ -2,7 +2,7 @@ from operator import le as less_than_equal_to_operator
 from operator import ge as greater_than_equal_to_operator
 
 
-def merge_sorted(sorted_array_1, sorted_array_2, comparison_operator):
+def _merge_sorted(sorted_array_1, sorted_array_2, comparison_operator):
     merged_sorted_array = []
     len1 = len(sorted_array_1)
     len2 = len(sorted_array_2)
@@ -33,33 +33,23 @@ def merge_sorted(sorted_array_1, sorted_array_2, comparison_operator):
     return merged_sorted_array
 
 
-def top_down_split_merge_sort(array, beg, end, desc=False):
+def top_down_split_merge_sort(array, beg, end, comparison_operator):
     """The section of array corresponding to beg:end is considered for sorting
     beg is inclusive and end is exclusive, so array[beg:end] is operative slice."""
     if len(array[beg:end]) <= 1:
         return
 
-    if desc:
-        comparison_operator = greater_than_equal_to_operator
-    else:
-        comparison_operator = less_than_equal_to_operator
-
     mid = (beg + end) // 2
-    top_down_split_merge_sort(array, beg, mid, desc)
-    top_down_split_merge_sort(array, mid, end, desc)
-    sorted_merge = merge_sorted(array[beg:mid], array[mid:end], comparison_operator)
+    top_down_split_merge_sort(array, beg, mid, comparison_operator)
+    top_down_split_merge_sort(array, mid, end, comparison_operator)
+    sorted_merge = _merge_sorted(array[beg:mid], array[mid:end], comparison_operator)
     array[beg:end] = sorted_merge
 
 
-def bottom_up_collect_merge_sort(array, desc=False):
+def bottom_up_collect_merge_sort(array, comparison_operator):
     array_len = len(array)
     if array_len <= 1:
         return
-
-    if desc:
-        comparison_operator = greater_than_equal_to_operator
-    else:
-        comparison_operator = less_than_equal_to_operator
 
     def width_gen(cutoff):
         a = 1
@@ -74,14 +64,31 @@ def bottom_up_collect_merge_sort(array, desc=False):
             sec2_end = min(i + 2 * width, array_len)
             if sec2_beg == sec2_end:
                 continue
-            sorted_merge = merge_sorted(array[sec1_beg:sec2_beg], array[sec2_beg:sec2_end], comparison_operator)
+            sorted_merge = _merge_sorted(array[sec1_beg:sec2_beg], array[sec2_beg:sec2_end], comparison_operator)
             array[sec1_beg:sec2_end] = sorted_merge
 
 
+def merge_sort(array, approach='top_down', desc=False):
+    if desc:
+        comparison_operator = greater_than_equal_to_operator
+    else:
+        comparison_operator = less_than_equal_to_operator
+    if approach == 'top_down':
+        array_len = len(array)
+        top_down_split_merge_sort(array, 0, array_len, comparison_operator)
+    elif approach == 'bottom_up':
+        bottom_up_collect_merge_sort(array, comparison_operator)
+    else:
+        raise NotImplementedError('{} type of merge sort not implemented.'.format(approach))
+
+
 if __name__ == '__main__':
-    l1, l2 = [1, 3, 6, 7], [2, 4, 5]
-    print(merge_sorted(l1, l2, less_than_equal_to_operator))
+    # l1, l2 = [1, 3, 6, 7], [2, 4, 5]
+    # print(_merge_sorted(l1, l2, less_than_equal_to_operator))
     test_list = [3, 6, 8, 24, 7, 11, 16]
     # top_down_split_merge_sort(test_list, 0, 7, True)
-    bottom_up_collect_merge_sort(test_list, True)
+    # bottom_up_collect_merge_sort(test_list, True)
+    merge_sort(test_list)
+    print(test_list)
+    merge_sort(test_list, desc=True)
     print(test_list)
