0397232636f2e681d143a08dc311789c27d0a68e
enlighter <mandal.sushovan92@gmail.com>
2020-04-10 16:04:42 +0530

    complete implementation of top-down merge sort

--- utils/mergesort.py
+++ utils/mergesort.py
@@ -1,4 +1,4 @@
-def top_down_merge_sorted(sorted_array_1, sorted_array_2):
+def top_down_merge_sorted(sorted_array_1, sorted_array_2, desc=False):
     merged_sorted_array = []
     len1 = len(sorted_array_1)
     len2 = len(sorted_array_2)
@@ -10,12 +10,20 @@ def top_down_merge_sorted(sorted_array_1, sorted_array_2):
         # print(merged_sorted_array)
         if (i < len1) and (j < len2):
             # print('both')
-            if sorted_array_1[i] <= sorted_array_2[j]:
-                merged_sorted_array.append(sorted_array_1[i])
-                i += 1
+            if not desc:
+                if sorted_array_1[i] <= sorted_array_2[j]:
+                    merged_sorted_array.append(sorted_array_1[i])
+                    i += 1
+                else:
+                    merged_sorted_array.append(sorted_array_2[j])
+                    j += 1
             else:
-                merged_sorted_array.append(sorted_array_2[j])
-                j += 1
+                if sorted_array_1[i] >= sorted_array_2[j]:
+                    merged_sorted_array.append(sorted_array_1[i])
+                    i += 1
+                else:
+                    merged_sorted_array.append(sorted_array_2[j])
+                    j += 1
         elif (i < len1) and (j >= len2):
             # print('j finished')
             merged_sorted_array.append(sorted_array_1[i])
@@ -29,10 +37,22 @@ def top_down_merge_sorted(sorted_array_1, sorted_array_2):
     return merged_sorted_array
 
 
-def merge_sort(array, beg, end):
-    pass
+def top_down_split_merge_sort(array, beg, end, desc=False):
+    """The section of array corresponding to beg:end is considered for sorting
+    beg is inclusive and end is exclusive, so array[beg:end] is operative slice."""
+    if len(array[beg:end]) <= 1:
+        return
+
+    mid = (beg + end) // 2
+    top_down_split_merge_sort(array, beg, mid, desc)
+    top_down_split_merge_sort(array, mid, end, desc)
+    sorted_merge = top_down_merge_sorted(array[beg:mid], array[mid:end], desc)
+    array[beg:end] = sorted_merge
 
 
 if __name__ == '__main__':
     l1, l2 = [1, 3, 6, 7], [2, 4, 5]
     print(top_down_merge_sorted(l1, l2))
+    test_list = [3, 6, 8, 24, 7, 11, 16]
+    top_down_split_merge_sort(test_list, 0, 7, True)
+    print(test_list)
