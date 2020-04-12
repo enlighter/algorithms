246b329261310877a5a13136f7736493e0eb8f00
enlighter <mandal.sushovan92@gmail.com>
2020-04-13 01:21:58 +0530

    correct average times instead of total time

--- test/merge_sort_test.py
+++ test/merge_sort_test.py
@@ -3,38 +3,38 @@ import random
 from utils.insertionsort import insertion_sort
 from utils.mergesort import merge_sort
 
-test_list = random.sample(range(10000), 52)
+test_list = random.sample(range(10000), 55)
 start = timer()
 for _ in range(10000):
     _ = insertion_sort(test_list)
 end = timer()
-insertion_sort_asc_time = end - start
+insertion_sort_asc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     merge_sort(test_list)
 end = timer()
-merge_sort_top_down_asc_time = end - start
+merge_sort_top_down_asc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     merge_sort(test_list, 'bottom_up')
 end = timer()
-merge_sort_bottom_up_asc_time = end - start
+merge_sort_bottom_up_asc_time = (end - start)/10000
 print("Asc. sort avg. times, insertion sort: {}, merge sort top down: {}, merge sort bottom up: {}".format(
     insertion_sort_asc_time, merge_sort_top_down_asc_time, merge_sort_bottom_up_asc_time))
 start = timer()
 for _ in range(10000):
     _ = insertion_sort(test_list, True)
 end = timer()
-insertion_sort_desc_time = end - start
+insertion_sort_desc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     merge_sort(test_list, 'top_down', True)
 end = timer()
-merge_sort_top_down_desc_time = end - start
+merge_sort_top_down_desc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     merge_sort(test_list, 'bottom_up', True)
 end = timer()
-merge_sort_bottom_up_desc_time = end - start
+merge_sort_bottom_up_desc_time = (end - start)/10000
 print("Desc. sort avg. times, insertion sort: {}, merge sort top down: {}, merge sort bottom up: {}".format(
     insertion_sort_desc_time, merge_sort_top_down_desc_time, merge_sort_bottom_up_desc_time))
--- test/n_squared_sort_test.py
+++ test/n_squared_sort_test.py
@@ -7,23 +7,23 @@ start = timer()
 for _ in range(10000):
     _ = bubble_sort(test_list)
 end = timer()
-bubble_sort_asc_time = end - start
+bubble_sort_asc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     _ = insertion_sort(test_list)
 end = timer()
-insertion_sort_asc_time = end - start
+insertion_sort_asc_time = (end - start)/10000
 print("Asc. sort avg. times, bubble sort: {}, insertion sort: {}".format(
     bubble_sort_asc_time, insertion_sort_asc_time))
 start = timer()
 for _ in range(10000):
     _ = bubble_sort(test_list, desc=True)
 end = timer()
-bubble_sort_desc_time = end - start
+bubble_sort_desc_time = (end - start)/10000
 start = timer()
 for _ in range(10000):
     _ = insertion_sort(test_list, desc=True)
 end = timer()
-insertion_sort_desc_time = end - start
+insertion_sort_desc_time = (end - start)/10000
 print("Desc. sort avg. times, bubble sort: {}, insertion sort: {}".format(
     bubble_sort_desc_time, insertion_sort_desc_time))
