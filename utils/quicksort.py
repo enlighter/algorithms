3c86b6365841d73fc9b2d3c5bc40e3c03696a2a5
enlighter <mandal.sushovan92@gmail.com>
2020-04-13 01:44:19 +0530

    update things within quicksort script

--- utils/quicksort.py
+++ utils/quicksort.py
@@ -6,10 +6,9 @@
 '''
 
 from random import randrange
-from time import process_time
 
 
-class quicksort(object):
+class QuickSort:
 	'''
 	This is a non-instantiable base class
 
@@ -20,31 +19,22 @@ class quicksort(object):
 	list to be sorted
 	'''
 	def __init__(self, to_sort):
-		if not type(to_sort) == list:
-			raise ValueError("'to_sort' needs to be a 'list' type object")
-		else:
-			for n in to_sort:
-				if not type(n) == int:
-					raise ValueError("This class only sorts 'int' type objects")
 		self.sorted = to_sort
 		self.last_pos = len(self.sorted) - 1
-		# Todo: in sub-class:-
-		# if self.last_pos > 0:
-		# 	self._process_(0, self.last_pos)
 
-	def _process_(self, beg, end):
+	def _process(self, lo, hi):
 		pass
 
-	def _partition_(self, beg, end):
-		pivot = self.sorted[beg]
+	def _partition(self, lo, hi):
+		pivot = self.sorted[lo]
 		# debug:
 		# print("pivot = %d" %pivot)
-		greater_than_pivot_start = beg + 1
+		greater_than_pivot_start = lo + 1
 
-		for read in range(beg+1, end+1):
-			#debug:
+		for read in range(lo + 1, hi + 1):
+			# debug:
 			# print("reading %d"%read)
-			if (self.sorted[read] <= pivot):
+			if self.sorted[read] <= pivot:
 				# debug:
 				# print("%d < %d" %(self.sorted[read], pivot))
 
@@ -57,7 +47,7 @@ class quicksort(object):
 
 		# debug:
 		# print("after processing: ",self.sorted)
-		self.sorted[beg], self.sorted[greater_than_pivot_start-1] = self.sorted[greater_than_pivot_start-1], self.sorted[beg]
+		self.sorted[lo], self.sorted[greater_than_pivot_start - 1] = self.sorted[greater_than_pivot_start - 1], self.sorted[lo]
 		# debug:
 		# print("after correcting pivot position: ",self.sorted)
 
@@ -66,44 +56,41 @@ class quicksort(object):
 		return greater_than_pivot_start-1
 
 	def __repr__(self):
-		return "quicksort.quicksort([])"
+		return "Class<QuickSort>"
 
 	def __str__(self):
-		return "Class<quicksort>"
+		return self.__repr__()
 
-class randomized_quicksort(quicksort):
+
+class RandomizedQuickSort(QuickSort):
 	# Todo: optimization for when there is repitition of elements
 	def __init__(self, to_sort):
-		start_time = process_time()
-		quicksort.__init__(self,to_sort)
-		self._process_(0, self.last_pos)
-		elapsed_time = process_time() - start_time
-		print("elapsed time = %f",elapsed_time)
+		QuickSort.__init__(self, to_sort)
+		self._process(0, self.last_pos)
 
-	def _process_(self, beg, end):
-		if beg == end:
+	def _process(self, lo, hi):
+		if lo == hi:
 			# debug:
 			# print("returning without any processing")
 			pass
 		else:
-			pivot = randrange(beg,end+1)
-			self.sorted[beg], self.sorted[pivot] = self.sorted[pivot],self.sorted[beg]
-			pivot = self._partition_(beg,end)
-			if pivot > beg:
-				self._process_(beg, pivot-1)
-			if pivot < end:
-				self._process_(pivot+1, end)
+			pivot = randrange(lo, hi + 1)
+			self.sorted[lo], self.sorted[pivot] = self.sorted[pivot], self.sorted[lo]
+			pivot = self._partition(lo, hi)
+			if pivot > lo:
+				self._process(lo, pivot - 1)
+			if pivot < hi:
+				self._process(pivot + 1, hi)
 
 	def __repr__(self):
-		return "quicksort.randomized_quicksort([])"
+		return "Class<RandomizedQuickSort>"
 
 	def __str__(self):
-		return "Class<randomized_quicksort>"
-
+		return self.__repr__()
 
 
 if __name__ == "__main__":
-	to_sort = [5,1,3,9,4,7,12,6,15,87,2,99,45,8,5]
+	test_list = [5, 1, 3, 9, 4, 7, 12, 6, 15, 87, 2, 99, 45, 8, 5]
 	# print(randomized_quicksort)
-	sort = randomized_quicksort(to_sort)
+	sort = RandomizedQuickSort(test_list)
 	print(sort.sorted)
