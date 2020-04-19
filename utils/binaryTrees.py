ef14cb32f5e01b1a2262009addf82536f430b48a
enlighter <mandal.sushovan92@gmail.com>
2020-04-19 19:23:51 +0530

    inherit the BST from binary tree with overloaded methods and debug insert

--- utils/binaryTrees.py
+++ utils/binaryTrees.py
@@ -5,39 +5,42 @@ class Node(object):
         self.right = None
 
 
-class BinaryFreeTree:
+class BinaryTree:
     def __init__(self, root):
         self.root = Node(root)
-        self.print_tree_selector = {
-            'preorder': self.preorder_print,
-            'inorder': self.inorder_print,
-            'postorder': self.postorder_print
+        self._print_tree_selector = {
+            'preorder': self._preorder_print,
+            'inorder': self._inorder_print,
+            'postorder': self._postorder_print
         }
+
+    def insert(self, new_val):
+        pass
 
     def search(self, find_val):
         """Return True if the value
         is in the tree, return
         False otherwise."""
-        return self.preorder_search(self.root, find_val)
+        return self.__preorder_search(self.root, find_val)
 
     def print_tree(self, traversal_mode='inorder'):
         """Print out all tree nodes
         as they are visited in
         a pre-order traversal."""
-        print_tree_method = self.print_tree_selector[traversal_mode]
+        print_tree_method = self._print_tree_selector[traversal_mode]
         return print_tree_method(self.root, "")
 
-    def preorder_search(self, start, find_val):
+    def __preorder_search(self, start, find_val):
         """Helper method - use this to create a
         recursive search solution."""
         if start is not None:
             if start.value == find_val:
                 return True
-            left_search = self.preorder_search(start.left, find_val)
+            left_search = self.__preorder_search(start.left, find_val)
             if left_search:
                 return True
             else:
-                return self.preorder_search(start.right, find_val)
+                return self.__preorder_search(start.right, find_val)
         ## Alternative
         # if start:
         #     if start.value == find_val:
@@ -56,35 +59,35 @@ class BinaryFreeTree:
         return traversal
 
     @classmethod
-    def preorder_print(cls, start, traversal):
+    def _preorder_print(cls, start, traversal):
         """Helper method - use this to create a
         recursive print solution."""
         if start is not None:
             traversal = cls.__populate_traversal(traversal, start.value)
-            traversal = cls.preorder_print(start.left, traversal)
-            traversal = cls.preorder_print(start.right, traversal)
+            traversal = cls._preorder_print(start.left, traversal)
+            traversal = cls._preorder_print(start.right, traversal)
         return traversal
 
     @classmethod
-    def inorder_print(cls, start, traversal):
+    def _inorder_print(cls, start, traversal):
         if start is not None:
-            traversal = cls.inorder_print(start.left, traversal)
+            traversal = cls._inorder_print(start.left, traversal)
             traversal = cls.__populate_traversal(traversal, start.value)
-            traversal = cls.inorder_print(start.right, traversal)
+            traversal = cls._inorder_print(start.right, traversal)
         return traversal
 
     @classmethod
-    def postorder_print(cls, start, traversal):
+    def _postorder_print(cls, start, traversal):
         if start is not None:
-            traversal = cls.postorder_print(start.left, traversal)
-            traversal = cls.postorder_print(start.right, traversal)
+            traversal = cls._postorder_print(start.left, traversal)
+            traversal = cls._postorder_print(start.right, traversal)
             traversal = cls.__populate_traversal(traversal, start.value)
         return traversal
 
 
-class BST(object):
+class BST(BinaryTree):
     def __init__(self, root):
-        self.root = Node(root)
+        super().__init__(root)
 
     def insert(self, new_val):
         current_node = self.root
@@ -92,8 +95,7 @@ class BST(object):
             if new_val < current_node.value and current_node.left is None:
                 current_node.left = Node(new_val)
                 return
-            # else part
-            if current_node.right is None:
+            if new_val >= current_node.value and current_node.right is None:
                 current_node.right = Node(new_val)
                 return
             if new_val < current_node.value:
@@ -143,7 +145,7 @@ class BST(object):
 
 if __name__ == '__main__':
     # Set up tree
-    tree = BinaryFreeTree(4)
+    tree = BinaryTree(4)
     tree.root.left = Node(2)
     tree.root.right = Node(5)
     tree.root.left.left = Node(1)
@@ -168,6 +170,7 @@ if __name__ == '__main__':
     tree.insert(2)
     tree.insert(1)
     tree.insert(3)
+    tree.insert(3)
     tree.insert(5)
 
     # Check search
@@ -175,3 +178,19 @@ if __name__ == '__main__':
     print(tree.search(4))
     # Should be False
     print(tree.search(6))
+
+    # def test(start):
+    #     if start is not None:
+    #         print(start.value)
+    #         print('left')
+    #         test(start.left)
+    #         print(start.value)
+    #         print('right')
+    #         test(start.right)
+    # test(tree.root)
+
+    # Test print_tree
+    # Should be 1-2-4-5-3
+    print('preorder traversal', tree.print_tree('preorder'))
+    print('inorder traversal', tree.print_tree('inorder'))
+    print('postorder traversal', tree.print_tree('postorder'))
